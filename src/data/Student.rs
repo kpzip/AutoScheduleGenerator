1f2ceb74c3f577b9e026747bfdc4191a9df3ac4b
kpzip <kpzipgames@gmail.com>
2023-11-06 22:11:43 -0800

    e

--- src/data/Student.rs
+++ src/data/Student.rs
@@ -26,24 +26,47 @@ list of Courses, the electives a student wishes to take
 use super::Course::*;
 use std::clone::Clone;
 use std::default::Default;
+use std::thread;
+
+static mut ID_COUNTER: u64 = 0;
 
 #[derive(Clone)]
 pub struct Student<'a> {
-    pub name: &'a str,
-    pub id: &'a str,
-    pub gradenum: i32,
-    pub element: &'a str,
-    pub mandatory_courses: Vec<Course<'a>>,
-    pub ranked_mandatory_courses: Vec<Vec<Course<'a>>>,
-    pub ranked_electives: Vec<Course<'a>>,
+    name: String,
+    id: u64,
+    gradenum: u8,
+    element: &'a str,
+    mandatory_courses: Vec<&'a Course<'a>>,
+    ranked_mandatory_courses: Vec<Vec<&'a Course<'a>>>,
+    ranked_electives: Vec<&'a Course<'a>>,
+}
+
+impl Student<'_> {
+    //only call on the main thread!!!!
+    pub fn new(name: String, grade: u8, element: &'static str) -> Student {
+        //really bad but it works I guess
+        assert_eq!(thread::current().name().unwrap(), "main");
+        let current_id;
+        unsafe {
+            current_id = ID_COUNTER;
+            ID_COUNTER = ID_COUNTER + 1;
+        }
+        Student {
+            name,
+            id: current_id,
+            gradenum: grade,
+            element,
+            ..Default::default()
+        }
+    }
 }
 
 impl<'a> Default for Student<'a> {
     fn default() -> Student<'a> {
         Student {
-            name: "",
-            id: "",
-            gradenum: -1,
+            name: String::from("John Doe"),
+            id: u64::MAX,
+            gradenum: 0, //imagine being in grade 0... idiot
             element: "",
             mandatory_courses: Vec::new(),
             ranked_mandatory_courses: Vec::new(),
